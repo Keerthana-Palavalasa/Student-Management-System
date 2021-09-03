eb4abc3b642cd4c044a2749ff1a75c0062d3d273
Keerthana Palavalasa <palavalasasaikeerthana@gmail.com>
2021-09-03 19:50:59 +0530

    Added project files

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>studentpage</title>
+	<link rel="stylesheet" href="css/all.min.css">
+	<link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div class="dropdown">
+	<nav>
+		<img style="height:60px;width:60px;border:2px solid #f39c12;margin-top: 2px;margin-left: 3px;"src="logo.png">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#"><img style="width: 40px" src="studentimg.png"><i class="fas fa-caret-circle-down"></i></a>
+			   <ul>
+		          <li><a href="#"><i class="fas fa-user-circle"></i> My Profile</a></li>
+		          <li><a href="#"><i class="fas fa-edit"></i> Edit Profile</a></li>
+		          <li><a href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li> 
+	           </ul>
+	        </li>
+		 </ul>
+    </nav>
+</div>
+<br>
+<br>
+<br>
+<br>
+<h2 style="color:black;text-align: center;font-size: 30px;">Student Management System</h2>
+<section style="margin:55px;" class="gallery" id="gallery">
+
+<div class="main">
+	<div class="box-container">
+		<div class="box">
+			<img src="classroom.png">
+			<div class="content">
+				<h3>PROFILE</h3>
+				<P>VIEW YOUR PROFILE</P>
+			</div>
+		</div>
+	</div>
+
+	<div class="box-container">
+		<div class="box">
+			<img src="attendance.jpg">
+			<div class="content">
+				<h3>Attendance</h3>
+				<p>VIEW YOUR ATTENDANCE</p>
+			</div>
+		</div>
+	</div>
+
+    <div class="box-container">
+		<div class="box">
+			<img src="assignments.png">
+			<div class="content">
+				<h3>Assignments</h3>
+				<P>VIEW YOUR ASSIGNMENTS</P>
+			</div>
+		</div>
+	</div>
+
+</div>
+</section>
+
+<div class="footer">
+	<br>
+	<h3 style="color:white;text-align: center;font-size: 20px;">All rights reserved@2021</h3>
+</div>
+</body>
+</html>
